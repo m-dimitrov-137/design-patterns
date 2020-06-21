249c82c364daa8c358f2fe69e88fe37dcd979faf
m-dimitrov-137 <63240685+m-dimitrov-137@users.noreply.github.com>
2020-06-21 14:24:33 +0300

    Delete State.java

--- src/state/State.java
+++
@@ -1,9 +0,0 @@
-package state;
-
-import observer.Observable;
-
-public interface State {
-	public void applyState(Context context);
-	public String getStateName();
-	public void resetState(Observable observable);
-}
